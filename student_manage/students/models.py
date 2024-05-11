a786016fcc3b9df3440a4f6b2b4e950a5e757028
ruperceniza <cenizaruper13@gmail.com>
2024-05-12 01:46:44 +0800

    Initial Commit

---
+++ student_manage/students/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    course = models.CharField(max_length=50, choices=[
+        ('BS-CS', 'Computer Science'),
+        ('BS-DS', 'Data Science'),
+        ('BS-IT', 'Information Technology'),
+        ('BS-IS', 'Information Systems')
+    ])
+    gender = models.CharField(max_length=6, choices=[
+        ('Male', 'Male'),
+        ('Female', 'Female')
+    ])
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
